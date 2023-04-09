bbdcb5736e2aef4ae583f0dd29ed5d68ac7a4088
Eduardo André <eduardoandre418@gmail.com>
2023-04-09 19:57:31 -0300

    sistema de clicar e desclicar inserido agora falta animação

--- scripty.js
+++ scripty.js
@@ -11,7 +11,7 @@ const imagens = [
 while(card_number > 14 || card_number%2 !=0 || card_number <= 3 ){
     card_number = prompt("quantas cartas pode ser de 2 a 14 cartas e somente par")
 }
-lista_aleatoria = []
+var lista_aleatoria = []
 let temp = imagens
 for(let j = ((Number(card_number)/2)-1);j > -1;j--){
     const aleatorio = Math.floor(Math.random()*(j+1));
@@ -28,35 +28,47 @@ function adicinorcartas(card_number){
     let cartas_conteudo = []
     for(let i = 0;i < card_number ; i++){
         mesa.innerHTML +=
-        `<div id="c${i}" class="card" data-test="card"  onclick="virarcarta(this)">
-        <img  class="front flip" data-test="face-up-image" src="${lista_aleatoria[i]}" alt="">
-        <img data-test="face-down-image" class="back"src="Arquivos/back.png" alt="">
+        `<div id="C${i}" class="card ${lista_aleatoria[i]}" data-test = 'card'  onclick="virarcarta(this)">
+        <img  class="front flip" data-test = 'face-up-image' src="${lista_aleatoria[i]}" alt="">
+        <img data-test= 'face-down-image' class="back"src="Arquivos/back.png" alt="">
         </div>`
     }
 }
 function virarcarta(carta){
-    const trocar1 = carta.querySelector('.front')
-    const trocar2 = carta.querySelector('.back')
-    trocar1.classList.toggle('flip')
-    trocar2.classList.toggle('flip')
+    const anterior = document.querySelector('.selecionado')
+    const front = carta.querySelector('.front')
+    const back = carta.querySelector('.back')
+    front.classList.remove('flip')
+    back.classList.toggle('flip')
     carta.classList.add('selecionado')
-    let selecionados = []
-    const comparador = document.querySelector(".Selecionado")
-    while(comparador != null){
-        selecionados.push(comparador)
-        comparador.classList.remove('Selecionado')
-        if(selecionados.length == 2){
-            if(comparador[0] !== comparador[1]){
-                alert(`diferentes`)
-            }
-            else{
-                
-            }
+    var Lista_selecionadas = document.querySelectorAll(".selecionado")
+    const a = Lista_selecionadas[Lista_selecionadas.length-2]
+    const fronta = a.querySelector(".front")
+    const backa = a.querySelector('.back')
+    const b = Lista_selecionadas[Lista_selecionadas.length-1]
+    const frontb = b.querySelector('.front')
+    const backb = b.querySelector('.back')
+    if(Lista_selecionadas.length >= 2){
+        if(a.innerHTML==b.innerHTML){
+            a.classList.add('certo')
+            a.classList.remove('selecionado')
+            a.removeAttribute('onclick')
+            b.classList.add('certo')
+            b.removeAttribute('onclick')
+            b.classList.remove('selecionado')
+            var Lista_selecionadas = []
+        }else if(a.innerHTML != b.innerHTML){
+            a.classList.remove('selecionado')
+            b.classList.remove('selecionado')
+            fronta.classList.add('flip')
+            frontb.classList.add('flip')
+            backa.classList.remove('flip')
+            backb.classList.remove('flip')
+            var Lista_selecionadas = []
         }
-        const comparador = document.querySelector(".Selecionados")
-        selecionados.push(comparador)
+    }else{
+        Lista_selecionadas = []
     }
-
 }
 adicinorcartas(card_number)
 
