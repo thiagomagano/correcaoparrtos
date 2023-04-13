87e0ab7ad7af716339f7ddd01f824741f29f60b9
Thiago Magano <thiagomagano1993@outlook.com>
2023-04-13 16:09:14 -0300

    fix:  data-test-imgs

--- scripty.js
+++ scripty.js
@@ -1,94 +1,97 @@
 let card_number = prompt("quantas cartas pode ser de 2 a 14 cartas e somente par")
 const imagens = [
-    'Arquivos/bobrossparrot.gif',
-    'Arquivos/explodyparrot.gif',
-    'Arquivos/fiestaparrot.gif',
-    'Arquivos/metalparrot.gif',
-    'Arquivos/revertitparrot.gif',
-    'Arquivos/tripletsparrot.gif',
-    'Arquivos/unicornparrot.gif',
+  'Arquivos/bobrossparrot.gif',
+  'Arquivos/explodyparrot.gif',
+  'Arquivos/fiestaparrot.gif',
+  'Arquivos/metalparrot.gif',
+  'Arquivos/revertitparrot.gif',
+  'Arquivos/tripletsparrot.gif',
+  'Arquivos/unicornparrot.gif',
 ]
 var lista_aleatoria = []
 var finalizados = 0;
 var tentativas = 0;
-while(card_number > 15 || card_number%2 !=0 || card_number <= 3 ){
-    card_number = prompt("quantas cartas pode ser de 2 a 14 cartas e somente par")
+while (card_number > 15 || card_number % 2 != 0 || card_number <= 3) {
+  card_number = prompt("quantas cartas pode ser de 2 a 14 cartas e somente par")
 }
 let temp = imagens
-while(card_number > 14 || card_number%2 !=0 || card_number <= 3 ){
-    card_number = prompt("quantas cartas pode ser de 2 a 14 cartas e somente par")
+while (card_number > 14 || card_number % 2 != 0 || card_number <= 3) {
+  card_number = prompt("quantas cartas pode ser de 2 a 14 cartas e somente par")
 }
-for(let j = ((Number(card_number)/2)-1);j > -1;j--){
-    const aleatorio = Math.floor(Math.random()*(j+1));
-    lista_aleatoria.push(temp[aleatorio]);
-    lista_aleatoria.push(temp[aleatorio]);
-    temp.splice(aleatorio,1)
+for (let j = ((Number(card_number) / 2) - 1); j > -1; j--) {
+  const aleatorio = Math.floor(Math.random() * (j + 1));
+  lista_aleatoria.push(temp[aleatorio]);
+  lista_aleatoria.push(temp[aleatorio]);
+  temp.splice(aleatorio, 1)
 }
 lista_aleatoria.sort(comparador)
-function comparador() { 
-	return Math.random() - 0.5;
+function comparador() {
+  return Math.random() - 0.5;
 }
-function adicinorcartas(card_number){
-    let mesa = document.getElementById('mesa');
-    let cartas_conteudo = []
-    for(let i = 0;i < card_number ; i++){
-        mesa.innerHTML +=
-        `<div id="C${i}" class="card ${lista_aleatoria[i]}" data-test = 'card'  onclick="virarcarta(this)">
-        <div class="front face" data-test = 'face-up-image' > <img  data-test = 'face-up-image' src="${lista_aleatoria[i]}" alt=""></div>
-        <div  class="back face" data-test = 'face-down-image'><img data-test = 'face-down-image' src="Arquivos/back.png" alt=""></div>
+function adicinorcartas(card_number) {
+  let mesa = document.getElementById('mesa');
+  let cartas_conteudo = []
+  for (let i = 0; i < card_number; i++) {
+    mesa.innerHTML +=
+      `<div id="C${i}" class="card ${lista_aleatoria[i]}" data-test = 'card'  onclick="virarcarta(this)">
+        <div class="front face"  > <img  data-test = 'face-up-image' src="${lista_aleatoria[i]}" alt=""></div>
+        <div  class="back face" ><img data-test = 'face-down-image' src="Arquivos/back.png" alt=""></div>
         </div>`
-    }
+  }
 }
-function reset(a,b,fronta,frontb,backa,backb){
+function reset(a, b, fronta, frontb, backa, backb) {
+  a.classList.remove('selecionado');
+  b.classList.remove('selecionado');
+  fronta.classList.add('flip');
+  frontb.classList.add('flip');
+  backa.classList.remove('flip');
+  backb.classList.remove('flip');
+  alert('porque n funciona')
+}
+function virarcarta(carta) {
+  const anterior = document.querySelector('.selecionado')
+  const front = carta.querySelector('.front')
+  const back = carta.querySelector('.back')
+  front.classList.remove('flip')
+  back.classList.add('flip')
+  carta.classList.add('selecionado')
+  var Lista_selecionadas = document.querySelectorAll(".selecionado")
+  const a = Lista_selecionadas[Lista_selecionadas.length - 2]
+  const fronta = a.querySelector(".front")
+  const backa = a.querySelector('.back')
+  const b = Lista_selecionadas[Lista_selecionadas.length - 1]
+  const frontb = b.querySelector('.front')
+  const backb = b.querySelector('.back')
+  function reset() {
     a.classList.remove('selecionado');
     b.classList.remove('selecionado');
     fronta.classList.add('flip');
     frontb.classList.add('flip');
     backa.classList.remove('flip');
     backb.classList.remove('flip');
-    alert('porque n funciona')}
-function virarcarta(carta){
-    const anterior = document.querySelector('.selecionado')
-    const front = carta.querySelector('.front')
-    const back = carta.querySelector('.back')
-    front.classList.remove('flip')
-    back.classList.add('flip')
-    carta.classList.add('selecionado')
-    var Lista_selecionadas = document.querySelectorAll(".selecionado")
-    const a = Lista_selecionadas[Lista_selecionadas.length-2]
-    const fronta = a.querySelector(".front")
-    const backa = a.querySelector('.back')
-    const b = Lista_selecionadas[Lista_selecionadas.length-1]
-    const frontb = b.querySelector('.front')
-    const backb = b.querySelector('.back')
-    function reset(){
-        a.classList.remove('selecionado');
-        b.classList.remove('selecionado');
-        fronta.classList.add('flip');
-        frontb.classList.add('flip');
-        backa.classList.remove('flip');
-        backb.classList.remove('flip');}
-    if(Lista_selecionadas.length >= 2){
-        if(a.innerHTML==b.innerHTML){
-            a.classList.add('certo')
-            a.classList.remove('selecionado')
-            a.removeAttribute('onclick')
-            b.classList.add('certo')
-            b.removeAttribute('onclick')
-            b.classList.remove('selecionado')
-            var Lista_selecionadas = []
-            tentativas = tentativas + 1;
-            finalizados = finalizados + 2;
-        }else if(a.innerHTML != b.innerHTML){
-            window.setTimeout(reset,1000)
-            var Lista_selecionadas = []
-            tentativas += 1;
-        }
-    }else{
-        Lista_selecionadas = []
+  }
+  if (Lista_selecionadas.length >= 2) {
+    if (a.innerHTML == b.innerHTML) {
+      a.classList.add('certo')
+      a.classList.remove('selecionado')
+      a.removeAttribute('onclick')
+      b.classList.add('certo')
+      b.removeAttribute('onclick')
+      b.classList.remove('selecionado')
+      var Lista_selecionadas = []
+      tentativas = tentativas + 1;
+      finalizados = finalizados + 2;
+    } else if (a.innerHTML != b.innerHTML) {
+      window.setTimeout(reset, 1000)
+      var Lista_selecionadas = []
+      tentativas += 1;
     }
-    if(finalizados >=card_number){
-        alert('VOCÊ GANHOU')}
+  } else {
+    Lista_selecionadas = []
+  }
+  if (finalizados >= card_number) {
+    alert('VOCÊ GANHOU')
+  }
 }
 
 adicinorcartas(card_number)
